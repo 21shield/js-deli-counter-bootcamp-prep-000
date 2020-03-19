469d5df91721e840df51bc9c271924a8225b2c8e
Netaly Ramirez <21netaly@gmail.com>
2020-03-19 01:09:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function nowServing(arr){
 function currentLine(lineArr){
   if(lineArr.length > 0){
     for (let i = 0; i < lineArr.length; i++){
-      return `The line is currently: 1.${lineArr[0]}, ${lineArr.join(i+1)}`
+      return `The line is currently: 1.${lineArr[0]}, ${lineArr[i]}`
     }
   }else{
     return "The line is currently empty."
